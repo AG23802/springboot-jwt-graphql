90193379a30d2fa6527578a95e3ce52787e299c1
Rexhin Hoxha <rexhin@icloud.com>
2025-08-22 22:28:11 +0200

    Fixed test

--- src/test/java/com/example/springbootjwtgraphql/TestControllerTest.java
+++ src/test/java/com/example/springbootjwtgraphql/TestControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.example.springbootjwtgraphql.api.rest.controllers.v2.TestController;
 import com.example.springbootjwtgraphql.application.security.JwtAuthenticationEntryPoint;
 import com.example.springbootjwtgraphql.application.security.JwtUtil;
 import com.example.springbootjwtgraphql.application.services.UserService;
@@ -15,7 +16,7 @@ import com.example.springbootjwtgraphql.application.services.UserService;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(TestControllerTest.class)
+@WebMvcTest(TestController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class TestControllerTest {
         @Autowired
@@ -32,7 +33,7 @@ public class TestControllerTest {
 
         @Test
         public void testSayHello_shouldReturnHelloWorld() throws Exception {
-                mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                mockMvc.perform(MockMvcRequestBuilders.get("/api/v2/hello"))
                                 .andExpect(status().isOk())
                                 .andExpect(content().json("[4 , 16, 9]"));
         }
