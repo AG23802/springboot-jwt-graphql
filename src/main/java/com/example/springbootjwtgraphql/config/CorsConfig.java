a94a6371c14c1f17b7cc2e4ab50ea3b8c385eb30
Rexhin Hoxha <rexhin@icloud.com>
2026-03-07 19:53:43 +0100

    refactor: Externalize CORS allowed origins in CorsConfig

--- src/main/java/com/example/springbootjwtgraphql/config/CorsConfig.java
+++ src/main/java/com/example/springbootjwtgraphql/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.springbootjwtgraphql.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,10 +12,14 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    // This reads the list from your YAML
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4173", "http://localhost:5173", "https://white-sand-0eeb3db03.2.azurestaticapps.net/"));
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
