5aa67b3d09f2083eb5818e897b96604d0059cc46
Rexhin Hoxha <rexhin@icloud.com>
2026-03-08 11:53:42 +0100

    feat: Convert EnvController to a REST controller

--- src/main/java/com/example/springbootjwtgraphql/api/rest/controllers/v1/EnvController.java
+++ src/main/java/com/example/springbootjwtgraphql/api/rest/controllers/v1/EnvController.java
@@ -2,9 +2,13 @@ package com.example.springbootjwtgraphql.api.rest.controllers.v1;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RestController
+@RequestMapping("api/v1")
 public class EnvController {
     @Value("${spring.profiles.active}")
     private String env;
