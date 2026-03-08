a034a2f8e03ef0751c7d6d045ffef92f1bdb7d88
Rexhin Hoxha <rexhin@icloud.com>
2026-03-08 12:56:26 +0100

    feat: Add logging test to EnvController for Application Insights

--- src/main/java/com/example/springbootjwtgraphql/api/rest/controllers/v1/EnvController.java
+++ src/main/java/com/example/springbootjwtgraphql/api/rest/controllers/v1/EnvController.java
@@ -1,5 +1,7 @@
 package com.example.springbootjwtgraphql.api.rest.controllers.v1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,15 @@ public class EnvController {
     @Value("${spring.profiles.active}")
     private String env;
 
+    private static final Logger logger = LoggerFactory.getLogger(EnvController.class);
+
     @GetMapping("/env")
     public Map<String, String> env() {
+        
+        logger.info("INFO: Logging test to Application Insights");
+        logger.warn("WARN: Logging test to Application Insights");
+        logger.error("ERROR: Logging test to Application Insights");
+
         return Map.of("env", env);
     }
 }
