8aa4495a411d0ee2d6ab7b4ca2efcdd6c4e015de
Rexhin Hoxha <rexhin@icloud.com>
2026-03-14 13:10:35 +0100

    feat: Remove Application Insights attachment

--- src/main/java/com/example/springbootjwtgraphql/SpringbootJwtGraphqlApplication.java
+++ src/main/java/com/example/springbootjwtgraphql/SpringbootJwtGraphqlApplication.java
@@ -1,6 +1,5 @@
 package com.example.springbootjwtgraphql;
 
-import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJwtGraphqlApplication {
 
 	public static void main(String[] args) {
-		ApplicationInsights.attach();
 		SpringApplication.run(SpringbootJwtGraphqlApplication.class, args);
 
 				Thread t = new Thread(() -> {
